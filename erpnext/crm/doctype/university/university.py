d67cf99a6edd4cd7a91f2426fc199d8ba36a9236
Jashan <jashanantal25@gmail.com>
2025-11-15 16:49:52 +0530

    done

---
+++ erpnext/crm/doctype/university/university.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class University(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		city: DF.Data | None
+		uniname: DF.Data | None
+	# end: auto-generated types
+
+	pass
